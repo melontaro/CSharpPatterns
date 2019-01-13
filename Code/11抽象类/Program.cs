ec3b04dcf3900c558caa7fa052f6a769fa274914
TouchAfflatus <axuyin@163.com>
2019-01-13 11:50:01 +0800

    抽象类|接口

---
+++ Code/11抽象类/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11抽象类
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ///Vegetables vegetables = new Vegetables();//抽象类无法实例化
+            Vegetables wheatgrass = new Wheatgrass("冰菜");//依然保留有多态的特性
+            wheatgrass.Breathe();
+        }
+    }
+    /// <summary>
+    /// 蔬菜
+    /// </summary>
+    abstract class Vegetables
+    {
+        public string Name { get; set; }
+        public Vegetables()
+        {
+
+        }
+        public Vegetables(string name)
+        {
+            this.Name = name;
+        }
+        protected string color;
+        /// <summary>
+        /// 抽象方法
+        /// </summary>
+        public abstract void Breathe();
+
+    }
+    /// <summary>
+    /// 冰菜/冰草
+    /// </summary>
+    class Wheatgrass : Vegetables
+    {
+        public Wheatgrass(string name) : base(name)
+        {
+            base.color = "绿色";
+
+        }
+
+        ///水泡
+        public string Blister { set; get; }
+
+        public override void Breathe()
+        {
+            System.Console.WriteLine("冰菜 呼吸");
+        }
+
+
+    }
+    /// <summary>
+    /// 胡萝卜
+    /// </summary>
+    class Carrot : Vegetables
+    {
+
+        public Carrot() : base()
+        {
+
+        }
+        public Carrot(string name) : base(name)
+        {
+
+        }
+
+        public override void Breathe()
+        {
+            System.Console.WriteLine("胡萝卜:呼吸");
+
+        }
+    }
+}
+
---
+++ Code/12接口/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12接口
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Actor cangJingKong = new Actor("苍井空");
+            cangJingKong.Perform();
+            cangJingKong.Conjure();
+
+            Magician liuQian = new Magician("刘谦");
+            liuQian.Conjure();
+            liuQian.Perform();
+        }
+    }
+    /// <summary>
+    /// 职业
+    /// </summary>
+   abstract class Profession
+    {
+        public Profession(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+    /// <summary>
+    /// 演员
+    /// </summary>
+ class Actor : Profession, IPerform,IConjure
+    {
+        public Actor(string name) : base(name)
+        {
+
+        }
+        public void Conjure()
+        {
+            System.Console.WriteLine(Name + ":我会变魔术(＾－＾)V~");
+        }
+
+        public void Perform()
+        {
+            System.Console.WriteLine(Name + ":我会演戏哦~");
+
+        }
+    }
+    /// <summary>
+    /// 魔术师
+    /// </summary>
+    class Magician : Profession, IConjure,IPerform
+    {
+        public Magician(string name) : base(name) { }
+        public void Conjure()
+        {
+            System.Console.WriteLine(Name+":我会变魔术");
+        }
+
+        public void Perform()
+        {
+            System.Console.WriteLine(Name+":我也会表演.");
+
+        }
+    }
+    /// <summary>
+    /// 技能接口
+    /// </summary>
+    interface IPerform//表演
+    {
+
+        void Perform();//表演
+    }
+    interface IConjure//变魔术
+    {
+        void Conjure();//变魔术
+    }
+}
