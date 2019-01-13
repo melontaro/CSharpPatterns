4c4a9fea41cceb15bd505fe9bd50d1e2aabafdd8
TouchAfflatus <axuyin@163.com>
2019-01-13 22:49:18 +0800

    集合

---
+++ Code/13集合/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace _13集合
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList list = new ArrayList();
+            list.Add("苍老师赛高!");
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(new Cat());//添加的时候做了装箱操作
+            arrayList.Add(new Dog());
+            arrayList.Add(1);
+            arrayList.Add("哈哈哈");
+            
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                System.Console.WriteLine(arrayList[i].ToString());
+            }
+
+            Dog dog = (Dog)arrayList[1];//拆箱操作
+
+        }
+    }
+    class Cat
+    {
+
+    }
+    class Dog
+    {
+
+    }
+}
